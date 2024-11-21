62b17c15b91b5225783e3b97aea2e3b1a8a725b2
Odra9 <sivori.leonardo09@gmail.com>
2024-11-21 15:29:28 +0100

    lab 21/11

--- include/LidarDriver.h
+++ include/LidarDriver.h
@@ -1,6 +1,28 @@
 #ifndef LIDAR_DRIVER
 #define LIDAR_DRIVER
 
-class LidarDriver {};
+#include <ostream>
+#include <vector>
 
+namespace LidarDriver {
+	class LidarDriver {
+		public:
+			LidarDriver(double degrees);
+
+			void new_scan(std::vector<double>& scan);
+			std::vector<double> get_scan();
+			void clear_buffer();
+			double get_distance(double degrees) const;
+
+		private:
+			double angular_res;
+			static const int BUFFER_DIM = 10;
+			int dim;
+			int tail, head;
+			double* buff;
+
+	};
+
+	std::ostream& operator<<(std::ostream& out, LidarDriver& ld);
+};
 #endif
--- src/LidarDriver.cpp
+++ src/LidarDriver.cpp
@@ -1,3 +1,44 @@
-#include <"../include/LidarDriver.h">
+#include "../include/LidarDriver.h"
 
-LidarDriver::LidarDriver() {}
+LidarDriver::LidarDriver::LidarDriver(double degrees) {
+	angular_res=degrees;
+	dim = int((180/angular_res + 1))*BUFFER_DIM;
+	head = 0;
+	tail = 0;
+	buff=nullptr;
+}
+
+void LidarDriver::LidarDriver::new_scan(std::vector<double>& scan) {
+	if(buff==nullptr) buff = new double[dim];
+	
+	
+} 
+
+std::vector<double> LidarDriver::LidarDriver::get_scan() {
+	if(buff==nullptr) return std::vector<double>{0};
+	
+	//save old scan
+	std::vector<double> r;
+	
+	for(int i=head*dim;i<(head+1)*dim;i++) {
+		r.push_back(buff[i]);
+	}
+	
+	//delete old scan
+	head = (head+1)%BUFFER_DIM;
+	
+	return r;
+}
+
+void LidarDriver::LidarDriver::clear_buffer() {
+	delete[] buff;
+	buff = nullptr;
+}
+
+double LidarDriver::LidarDriver::get_distance(double degrees) const {
+	if(buff==nullptr) return 0;
+	
+	int index = tail*BUFFER_DIM + int(angular_res/degrees);
+	
+	return buff[index];
+}
